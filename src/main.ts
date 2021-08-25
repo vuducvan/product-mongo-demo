c9deb4fbf6a73775e41323ff3eb45219eb6fdda2
Smallbook20 <vuducvan999@gmail.com>
2021-08-25 10:14:21 +0700

    init commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Product Demo Nest API')
+    .setDescription('API for project about product')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+  await app.listen(3000);
+}
+bootstrap();
